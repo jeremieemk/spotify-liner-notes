9af2de90e5bcc92f64947983e80b6072bc477974
jeremiemk <jeremieemk@gmail.com>
2020-08-11 15:42:29 +0100

    login and data fetching OK

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import queryString from "query-string";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    let parsed = queryString.parse(window.location.search);
+    setAccessToken(parsed.access_token);
+  }, [window.location]);
+
+  useEffect(() => {
+    accessToken &&
+      fetch("https://api.spotify.com/v1/me", {
+        headers: { Authorization: "Bearer " + accessToken },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setData(data);
+        });
+  }, [accessToken]);
+
+  function handleSignInClick() {
+    window.location = "https://spotify-labels-backend.herokuapp.com/login";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!accessToken && (
+        <button onClick={handleSignInClick}>Sign in with Spotify</button>
+      )}
+      {accessToken && (
+        <h2>
+          Hi {data && data.display_name}, you have{" "}
+          {data && data.followers.total} followers on Spotify
+        </h2>
+      )}
     </div>
   );
 }
