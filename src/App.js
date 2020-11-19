ed39701401316f2c856b26dc0e02da36067aab8b
jeremiemk <jeremieemk@gmail.com>
2020-11-19 19:27:55 +0000

    changed redirect uri

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   function handleSignInClick() {
     const client_id = process.env.REACT_APP_SPOTIFY_KEY
     const redirect_uri = process.env.NODE_ENV === "production"
-        ? "https://spotify-now-playing-discogs.netlify.app"
+        ? "https://spotify-liner-notes.netlify.app"
         : "http://localhost:3000" ;
     const scope = "streaming user-read-email user-read-private user-library-read user-read-playback-state user-modify-playback-state"
     window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scope}&show_dialog=true`
